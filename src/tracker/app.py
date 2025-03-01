c8c666b37b437f4050eb239c3c5adefcb0a4bd05
thompsonDevs <90111507+thompsonDevs@users.noreply.github.com>
2025-03-01 15:38:17 +0000

    structure app

---
+++ src/tracker/app.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+def scrape_bitpanda():
+    return
+
+def scrape_exodus():
+    return
+
+def scrape_tradingview():
+    return
+
+def update_funds():
+    return
+
+def send_alert():
+    return
